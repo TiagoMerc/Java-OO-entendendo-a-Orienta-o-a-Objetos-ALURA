89c94962fd4b522a1505ab2c0d8f52992476cea4
TiagoMerc <tmrrosario9@gmail.com>
2022-09-17 23:27:27 -0300

    Referências vs Objetos

--- README.md
+++ README.md
@@ -19,3 +19,29 @@ Instrutor: [Paulo Silveira](https://www.linkedin.com/in/paulosilveira)
 **Instanciação, atributos e referências**
 
 ![Instanciação, atributos e referências](./imgs/prints/InstanciacaoAtributosReferencias.png)
+
+**Nessa aula aprendemos sobre atributos e criação de objetos.**
+
+Vimos:
+
+- O que é uma classe
+
+- Como criar uma classe
+
+- Como criar um objeto ou instância de uma classe
+
+- O que são atributos
+
+- Como definir e visualizar valores de atributos
+
+- Como funciona a atribuição de uma referência à uma variável
+
+Vamos ver mais sobre as classes na próxima aula! Agora não só suas características mas também seu comportamento. Vamos que só está no começo! Espero você lá!
+
+**Referências vs Objetos**
+
+![Referências Objetos](./imgs/prints/ReferenciaObjetos.png)
+
+**Referências**
+
+![Referências](./imgs/prints/Referencia.png)
--- src/CriaConta.java
+++ src/CriaConta.java
@@ -4,5 +4,32 @@ public class CriaConta {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
         System.out.println(primeiraConta.saldo);
+    
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+        
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 300;
+        
+        segundaConta.agencia = 146;
+        System.out.println("primeria conta tem " + primeiraConta.saldo);
+        System.out.println("segunda conta tem " + segundaConta.saldo);
+        
+        System.out.println(primeiraConta.agencia);
+        
+        segundaConta.agencia = 146;
+        System.out.println("Agora a segunda conta esta na agencia " + segundaConta.agencia) ;   
+        
+        if(primeiraConta == segundaConta) {
+        	System.out.println("mesma conta");
+        }else {
+        	System.out.println("contas diferentes");
+        }
+        //Referência para outra conta
+        
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
+
+
     }
 }
